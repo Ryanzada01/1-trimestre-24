a0614e5b064c778c9e09673a6c7b6e5ff0346cd3
Ryanzada01 <106113489+Ryanzada01@users.noreply.github.com>
2024-04-02 09:58:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivol = new Date("2024-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivol = new Date("2024-10-05T00:00:00");
+const tempoObjetivol = new Date)
 
 contadores[0].textContent = calculaTempo(tempoObjetivol);
 
@@ -25,4 +26,12 @@ function calculaTempo(tempoObjetivo){
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
